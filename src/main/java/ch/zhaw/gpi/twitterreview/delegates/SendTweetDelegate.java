d9a9c1b6bc2c9d1490717e4971b062af4e86489c
lukes <lukes@Yoga-900>
2018-12-10 16:13:25 +0100

    8.2.5

---
+++ src/main/java/ch/zhaw/gpi/twitterreview/delegates/SendTweetDelegate.java
@@ -0,0 +1,35 @@
+package ch.zhaw.gpi.twitterreview.delegates;
+
+import ch.zhaw.gpi.twitterreview.services.TwitterService;
+import javax.inject.Named;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Implementation des Service Tasks "Tweet Senden"
+ * 
+ * @author lukes
+ */
+@Named("sendTweetAdapter")
+public class SendTweetDelegate implements JavaDelegate {
+
+// Verdrahten des TwitterService
+    @Autowired
+    private TwitterService twitterService;
+/**
+* Postet einen Tweet mit dem gewünschten Text
+*
+* @param de Objekt, welches die Verknüpfung zur Process Engine und zur aktuellen Execution enthält
+* @throws Exception
+*/
+    @Override
+    public void execute(DelegateExecution de) throws Exception {
+        // Prozessvariable tweetContent wird ausgelesen
+        String tweetContent = (String) de.getVariable("tweetContent");
+        // Dieser Text wird dem Twitter Service an die Methode updateStatus übergeben
+        twitterService.updateStatus(tweetContent);
+
+    }
+    
+}
